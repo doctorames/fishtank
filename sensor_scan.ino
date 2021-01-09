7126b1e330b9366c7ca07b1d66f67e667e6b0789
daniel-ames <daniel.ames@hp.com>
2021-01-09 09:33:44 -0600

    Make it spit out the address in c-style byte array definition format so alls you gotta do is copy-paste it into code

--- sensor_scan.ino
+++ sensor_scan.ino
@@ -1,11 +1,6 @@
 //#include <OneWire.h>  <-- OneWire.h is already included in DallasTemperature.h
 #include <DallasTemperature.h>
 
-// purple: 286BB707D6013CAC
-// white:  28902D07D6013C2C
-// green:  28162C07D6013CB9
-// orange: 284F4107B6013C59
-
 
 // GPIO pins
 #define ONE_WIRE_BUS 16
@@ -13,15 +8,19 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-
+// Outputs the address in c-style byte array format.
+// For example:
+//   {0x28, 0xF8, 0x13, 0x07, 0xB6, 0x01, 0x3C, 0xCD}
 void printDeviceAddress(DeviceAddress deviceAddress) {
-  
+  Serial.print("{");
   for (uint8_t i = 0; i < 8; i++) {
-    
-    if (deviceAddress[i] < 16) 
+    Serial.print("0x");
+    if (deviceAddress[i] < 16)
       Serial.print("0");
       Serial.print(deviceAddress[i], HEX);
+      if(i != 7) Serial.print(", ");
   }
+  Serial.print("}");
 }
 
 void setup()
